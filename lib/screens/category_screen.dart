b37ddb7d96827f240d4e71109585a372e5b90b94
Aman Kumar <amankumarandpin@gmail.com>
2025-10-23 02:14:08 +0530

    Recovered newer version of category_screen.dart

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flash_feed/screens/home_page.dart';
-import 'package:flash_feed/utils/util.dart';
 import 'package:flash_feed/widgets/category_card.dart';
 import 'package:flash_feed/widgets/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
@@ -34,112 +33,166 @@ class _CategoryScreenState extends State<CategoryScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: [
-            Row(
-              children: const [
-                CategoryCard(
-                  icon: Icons.business_center_outlined,
-                  title: "Business",
-                  backgroundColor: Color(0xFFE7F8EA),
-                  iconColor: Colors.green,
-                ),
-                SizedBox(width: 10),
-                CategoryCard(
-                  icon: Icons.restaurant_outlined,
-                  title: "Food & Culture",
-                  backgroundColor: Color(0xFFEAF2FF),
-                  iconColor: Colors.blue,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: const [
-                CategoryCard(
-                  icon: Icons.coffee_outlined,
-                  title: "Office Productivity",
-                  backgroundColor: Color(0xFFFFE8E7),
-                  iconColor: Colors.red,
-                ),
-                SizedBox(width: 10),
-                CategoryCard(
-                  icon: Icons.account_balance_wallet_outlined,
-                  title: "Finance & Accounting",
-                  backgroundColor: Color(0xFFFFF4E5),
-                  iconColor: Colors.orange,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: const [
-                CategoryCard(
-                  icon: Icons.computer_outlined,
-                  title: "IT & Software",
-                  backgroundColor: Color(0xFFEAF2FF),
-                  iconColor: Colors.blue,
-                ),
-                SizedBox(width: 10),
-                CategoryCard(
-                  icon: Icons.work_outline,
-                  title: "Office Productivity",
-                  backgroundColor: Color(0xFFF8EAF8),
-                  iconColor: Colors.purple,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: const [
-                CategoryCard(
-                  icon: Icons.psychology_outlined,
-                  title: "Personal Development",
-                  backgroundColor: Color(0xFFEAF2FF),
-                  iconColor: Colors.indigo,
-                ),
-                SizedBox(width: 10),
-                CategoryCard(
-                  icon: Icons.design_services_outlined,
-                  title: "Design",
-                  backgroundColor: Color(0xFFE7F8EA),
-                  iconColor: Colors.green,
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Row(
-              children: const [
-                CategoryCard(
-                  icon: Icons.camera_alt_outlined,
-                  title: "Photography & Video",
-                  backgroundColor: Color(0xFFEAF2FF),
-                  iconColor: Colors.blue,
-                ),
-                SizedBox(width: 10),
-                CategoryCard(
-                  icon: Icons.favorite_outline,
-                  title: "Health & Fitness",
-                  backgroundColor: Color(0xFFE7F8EA),
-                  iconColor: Colors.green,
-                ),
-              ],
-            ),
-            SizedBox(height: 15),
-            CustomElevatedButton(
-              text: "Get Started",
-              onTap: () {
-                Navigator.pushAndRemoveUntil(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage()),
-                  (route) => false,
-                );
-              },
-              txtColor: secondaryShade,
-              btnHeight: 48,
-              btnWidth: 180,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.business_center_outlined,
+                    title: "Business",
+                    backgroundColor: const Color(0xFFE7F8EA),
+                    iconColor: Colors.green,
+                    isSelected: _selectedCategories.contains("Business"),
+                    onTap: () => toggleCategory("Business"),
+                  ),
+                  const SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.restaurant_outlined,
+                    title: "Food & Culture",
+                    backgroundColor: const Color(0xFFEAF2FF),
+                    iconColor: Colors.blue,
+                    isSelected: _selectedCategories.contains("Food & Culture"),
+                    onTap: () => toggleCategory("Food & Culture"),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.coffee_outlined,
+                    title: "Office Productivity",
+                    backgroundColor: const Color(0xFFFFE8E7),
+                    iconColor: Colors.red,
+                    isSelected: _selectedCategories.contains(
+                      "Office Productivity",
+                    ),
+                    onTap: () => toggleCategory("Office Productivity"),
+                  ),
+                  const SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.account_balance_wallet_outlined,
+                    title: "Finance & Accounting",
+                    backgroundColor: const Color(0xFFFFF4E5),
+                    iconColor: Colors.orange,
+                    isSelected: _selectedCategories.contains(
+                      "Finance & Accounting",
+                    ),
+                    onTap: () => toggleCategory("Finance & Accounting"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.computer_outlined,
+                    title: "IT & SOFTWARE",
+                    backgroundColor: Color(0xFFEAF2FF),
+                    iconColor: Colors.blue,
+                    isSelected: _selectedCategories.contains("IT & SOFTWARE"),
+                    onTap: () => toggleCategory("IT & SOFTWARE"),
+                  ),
+                  const SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.water_drop_outlined,
+                    title: "Weather",
+                    backgroundColor: Color(0xFFF8EAF8),
+                    iconColor: Colors.purple,
+                    isSelected: _selectedCategories.contains("weather"),
+                    onTap: () => toggleCategory("weather"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.psychology_outlined,
+                    title: "Personal Development",
+                    backgroundColor: Color(0xFFEAF2FF),
+                    iconColor: Colors.indigo,
+                    isSelected: _selectedCategories.contains(
+                      "Personal Development",
+                    ),
+                    onTap: () => toggleCategory("Personal Development"),
+                  ),
+                  const SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.design_services_outlined,
+                    title: "Design",
+                    backgroundColor: Color(0xFFE7F8EA),
+                    iconColor: Colors.green,
+                    isSelected: _selectedCategories.contains("Design"),
+                    onTap: () => toggleCategory("Design"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.camera_alt_outlined,
+                    title: "Photography & Video",
+                    backgroundColor: Color(0xFFEAF2FF),
+                    iconColor: Colors.blue,
+                    isSelected: _selectedCategories.contains(
+                      "Photography & Video",
+                    ),
+                    onTap: () => toggleCategory("Photography & Video"),
+                  ),
+                  SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.calendar_month_outlined,
+                    title: "Lifestyle",
+                    backgroundColor: Color(0xFFEAF2FF),
+                    iconColor: Colors.blue,
+                    isSelected: _selectedCategories.contains("Lifestyle"),
+                    onTap: () => toggleCategory("Lifestyle"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  CategoryCard(
+                    icon: Icons.favorite_border_outlined,
+                    title: "Health & Fitness",
+                    backgroundColor: Color(0xFFE7F8EA),
+                    iconColor: Colors.blue,
+                    isSelected: _selectedCategories.contains(
+                      "Health & Fitness",
+                    ),
+                    onTap: () => toggleCategory("Health & Fitness"),
+                  ),
+                  SizedBox(width: 10),
+                  CategoryCard(
+                    icon: Icons.calendar_month_outlined,
+                    title: "Development",
+                    backgroundColor: const Color(0xFFFFF4E5),
+                    iconColor: Colors.orange,
+                    isSelected: _selectedCategories.contains("Development"),
+                    onTap: () => toggleCategory("Development"),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              CustomElevatedButton(
+                text: "Save & next",
+                onTap: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomePage()),
+                    (route) => false,
+                  );
+                },
+                txtColor: Colors.white,
+                btnHeight: 48,
+                btnWidth: 180,
+              ),
+            ],
+          ),
         ),
       ),
     );
